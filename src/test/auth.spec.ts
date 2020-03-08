bddd154ae191217a1a0be256e64e9cdaabc1c18e
ohadsahar <46564719+ohadsahar@users.noreply.github.com>
2020-03-08 22:37:09 +0200

    added jest

---
+++ src/test/auth.spec.ts
@@ -0,0 +1,16 @@
+import bootstrapDb from '../config/db.config';
+import { AuthService } from '../api/services/auth.service';
+import { Container } from 'typedi';
+
+const authService = Container.get(AuthService);
+
+const phoneNumber = '+9720526477497';
+beforeAll(async () => {
+    await bootstrapDb();
+})
+
+test('Send sms to client', async () => {
+    const result = await authService.sendSms(phoneNumber);
+    expect(result).toHaveProperty('accountSid')
+});
+
